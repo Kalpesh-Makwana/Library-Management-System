378c3ed2863c5979e59cfd2bce76f15bcfe66043
Kalpesh <kalpeshmakwna2610@gmail.com>
2020-03-31 19:03:41 +0530

    created account app

---
+++ LMS/account/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+class Index(TemplateView):
+    '''template for display when user come'''
+    template_name="account/index.html"
